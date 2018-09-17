e484ed9281be2837dbf537b3e60dd090d878386f
gnunn1 <gerald.b.nunn@gmail.com>
2018-09-17 16:01:42 -0400

    Update for bootstrap service

--- support/strimzi-master.sh
+++ support/strimzi-master.sh
@@ -1,5 +1,7 @@
 export STRIMZI_HOME=/home/gnunn/Development/kafka/strimzi-kafka-operator
 
+oc new-project sko --display-name="Seat Registration"
+
 sed -i 's/namespace: .*/namespace: sko/' ${STRIMZI_HOME}/examples/install/cluster-operator/*RoleBinding*.yaml
 oc apply -f ${STRIMZI_HOME}/examples/install/cluster-operator -n sko
 oc apply -f ${STRIMZI_HOME}/examples/templates/cluster-operator -n sko
